09b4421ef9fc0cd0af927e1f174e40ce041bb11a
Vincent893 <vicentediazotero@gmail.com>
2026-04-09 01:54:55 -0400

    Update funciones.py
    pequeñas correcciones al archivo funciones.py

--- utils/funciones.py
+++ utils/funciones.py
@@ -12,29 +12,24 @@ ENV_PATH = BASE_DIR / ".env"
 load_dotenv(dotenv_path=ENV_PATH)
 
 #Obtener variables de entorno
-USER = os.getenv('SUPABASE_USER')
-PASSWORD = os.getenv('SUPABASE_PASSWORD')
-HOST = os.getenv('SUPABASE_HOST')
-PORT = os.getenv('SUPABASE_PORT')
-DBNAME = os.getenv('SUPABASE_DBNAME')
+database_url = os.getenv('SUPABASE_DATABASE_URL')
 
-uri_supabase = f'postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}'
-engine = create_engine(uri_supabase)
+engine = create_engine(database_url)
 
 #Creamos funcion para rellenar valores nulos segun datos de nuevos dataframes
-def rescatar_nulos(df_nuevo, table_name, engine):
+def rescatar_nulos(df_nuevo, engine):
     """
     Compara un DataFrame con una tabla de Supabase y rellena SOLO los valores 
     que actualmente son NULL en la base de datos.
     """
     try:
         #Subir el DataFrame a una tabla temporal en Supabase
-        temp_table_name = f"temp_{table_name}"
+        temp_table_name = f"temp_indicators"
         df_nuevo.to_sql(temp_table_name, engine, if_exists='replace', index=False)
 
         #Definir la consulta SQL de actualización 
         query = text(f"""
-            UPDATE {table_name} AS principal
+            UPDATE indicators AS principal
             SET value = temporal.value
             FROM {temp_table_name} AS temporal
             WHERE principal.country_code = temporal.country_code
@@ -44,12 +39,15 @@ def rescatar_nulos(df_nuevo, table_name, engine):
         """)
 
         #Ejecutar transacción
-        with engine.begin() as connection:
-            result = connection.execute(query)
+        with engine.begin() as conn:
+            result = conn.execute(query)
 
         #Borrar la tabla temporal
-        with engine.begin() as connection:
-            connection.execute(text(f"DROP TABLE {temp_table_name};"))
+        with engine.begin() as conn:
+            conn.execute(text(f"DROP TABLE {temp_table_name};"))
+
+        print(f"Se han actualizado {result.rowcount} valores nulos")
 
     except Exception as e:
         print(f"Error durante el proceso: {e}")
+
