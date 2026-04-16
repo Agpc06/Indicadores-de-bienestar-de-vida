977116ea7ed46acb00b2d08d2b0dc1b046a26e06
AngelDavRH <reina.angel31788335@gmail.com>
2026-04-16 14:12:53 -0400

    Update 08_Impacto_del_CO2_en_la_Esperanza_de_Vida.py

--- pages/08_Impacto_del_CO2_en_la_Esperanza_de_Vida.py
+++ pages/08_Impacto_del_CO2_en_la_Esperanza_de_Vida.py
@@ -24,16 +24,42 @@ if not df_impacto.empty:
     ).dropna().reset_index()
 
     if not df_pivot.empty:
-        fig_obj = px.scatter(
-            df_pivot,
-            x=ind_co2,
-            y=ind_vida,
-            color="Nombre Pais",
-            trendline="ols",
-            hover_name="Año",
-            template="plotly_dark",
-            labels={ind_co2: "Emisiones de CO2 (ton. per cápita)", ind_vida: "Esperanza de VIda (años)"}
+        # Filtro de país
+        st.sidebar.header("Filtros")
+        paises_en_pivot = sorted(df_pivot["Nombre Pais"].unique())
+        paises_sel_grafico = st.sidebar.multiselect(
+            "Seleccionar países para el análisis:",
+            options=paises_en_pivot,
+            default=paises_en_pivot
         )
+
+        # Filtro de año
+        anios_en_pivot = sorted(df_pivot["Año"].unique())
+        rango_anios_grafico = st.sidebar.slider(
+            "Rango de Años:",
+            min_value=int(min(anios_en_pivot)),
+            max_value=int(max(anios_en_pivot)),
+            value=(int(min(anios_en_pivot)), int(max(anios_en_pivot)))
+        )
+
+        # Aplicamos los filtros
+        df_filtrado_final=df_pivot[
+            (df_pivot["Nombre Pais"].isin(paises_sel_grafico)) &
+            (df_pivot["Año"].between(rango_anios_grafico[0], rango_anios_grafico[1]))
+        ]
+
+        if not df_filtrado_final.empty:
+            fig_obj = px.scatter(
+                df_filtrado_final,
+                x=ind_co2,
+                y=ind_vida,
+                color="Nombre Pais",
+                trendline="ols",
+                hover_name="Año",
+                template="plotly_dark",
+                labels={ind_co2:"Emisiones de CO2 (ton. per cápita)", ind_vida:"Esperanza de Vida (años)"}
+            )
+
         st.plotly_chart(fig_obj, use_container_width=True)
 
     else:
