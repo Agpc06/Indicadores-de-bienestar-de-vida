d2b8b6a31a60b7ed4942b379eb8dbdc45221b381
Vincent893 <vicentediazotero@gmail.com>
2026-04-15 22:38:27 -0400

    Creacion de introduccion.py
    Se creo pagina de introducción en app de streamlit

---
+++ pages/01_Introducción.py
@@ -0,0 +1,16 @@
+import streamlit as st 
+
+st.set_page_config(
+    page_title="Introducción",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+st.title('📊 Análisis del Impacto Urbano en la Calidad de Vida y la Sostenibilidad Ambiental de Latinoamérica')
+st.markdown('---')
+
+st.markdown('En las últimas décadas, la humanidad ha comprendido que el verdadero éxito de una nación no se mide solo por su riqueza material, sino por su capacidad para ofrecer una vida digna y saludable a sus ciudadanos sin comprometer los recursos naturales del futuro. Ahora, el desarrollo es un equilibrio entre economía, sociedad y medio ambiente.')
+
+st.markdown('En este panorama, Latinoamérica se presenta como una región de cambios. Representando aproximadamente el 8% de la población mundial según la CEPAL (Comisión Económica para América Latina y el Caribe), la región enfrenta el reto de gestionar procesos en la urbanización mientras intenta preservar una de las biodiversidades más ricas del planeta. Sin embargo, los indicadores de desarrollo tradicionales suelen ocultar las tensiones que existen entre el crecimiento de las ciudades y el bienestar real.')
+
+st.markdown('Para profundizar en esta realidad, este estudio utiliza los Indicadores del Desarrollo Mundial del Banco Mundial como un lente analítico. Al procesar datos que abarcan desde 1963 hasta 2023, se busca transformar cifras globales en conocimiento estratégico, permitiendo identificar si el avance de la región es realmente sostenible o si los logros en calidad de vida están generando una deuda ambiental irreversible.')
